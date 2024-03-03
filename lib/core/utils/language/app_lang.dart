ed0fcf068d59e58393823c45bd0d1bc3019393e9
khaled-dreat <dreatkhaled@gmail.com>
2024-03-03 17:47:58 +0200

    Add App Lang File

--- lib/core/utils/language/app_lang.dart
+++ lib/core/utils/language/app_lang.dart
@@ -1,5 +1,6 @@
-/*
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class AppLang {
   ///  [currentLang] return en or ar
@@ -10,19 +11,18 @@ class AppLang {
   static void setLang(BuildContext context, Locale locale) =>
       context.setLocale(locale);
 
-  static const String keySHOWLANG = 'SHOWLANG';
+  static const String keyShowLang = 'SHOWLANG';
+  Box<bool> haivSelectedLang = Hive.box<bool>(keyShowLang);
 
   /// [setShowLang] save key show lang  is false stop show selected lang
-  static Future<void> setShowLang() async {
-    final SharedPreferences pref = await SharedPreferences.getInstance();
-    await pref.setBool(keySHOWLANG, false);
+  Future<void> setShowLang() async {
+    Box<bool> haivSelectedLang = Hive.box<bool>(keyShowLang);
+    await haivSelectedLang.put(keyShowLang, false);
   }
 
   /// [showSelectLang] get data saved in locale device
   static Future<bool> get showSelectLang async {
-    final SharedPreferences pref = await SharedPreferences.getInstance();
-    //* [pref.getBool] default true mean no choose before
-    return pref.getBool(keySHOWLANG) ?? true;
+    Box<bool> haivSelectedLang = Hive.box<bool>(keyShowLang);
+    return haivSelectedLang.get(keyShowLang) ?? true;
   }
 }
-*/*/
