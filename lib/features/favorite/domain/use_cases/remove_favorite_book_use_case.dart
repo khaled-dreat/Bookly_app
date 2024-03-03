684f8d0a454079e13ec66717424622fb07357db0
khaled-dreat <dreatkhaled@gmail.com>
2024-03-03 12:02:24 +0200

    Add removeFavoriteBooks in repo and usecase

---
+++ lib/features/favorite/domain/use_cases/remove_favorite_book_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/usecase/use_case.dart';
+import '../../../../core/utils/constant/app_failure.dart';
+import '../repo/repo_favorite.dart';
+
+class AddFavoriteBooksUseCase extends UseCase<void, String> {
+  final FavoriteRepo favoriteRepo;
+
+  AddFavoriteBooksUseCase({required this.favoriteRepo});
+
+  @override
+  Future<Either<Failure, void>> call([String? param]) async {
+    return await favoriteRepo.removeFavoriteBooks(book: param!);
+  }
+}
