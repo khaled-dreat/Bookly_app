fc3419bca167cc50e54a67e7b921329893d8dbe9
khaled-dreat <dreatkhaled@gmail.com>
2024-03-03 17:50:52 +0200

    Add  ChoiceLang Enum

--- lib/features/drawer/presentation/manger/lang/lang_cubit.dart
+++ lib/features/drawer/presentation/manger/lang/lang_cubit.dart
@@ -1,7 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../../core/utils/language/app_lang_key.dart';
+
 part 'lang_state.dart';
+
+enum ChoiceLang {
+  en(AppLangKey.english),
+  ar(AppLangKey.arabic);
+
+  final String nameLang;
+  const ChoiceLang(this.nameLang);
+}
 
 class LangCubit extends Cubit<LangState> {
   LangCubit() : super(LangInitial());
