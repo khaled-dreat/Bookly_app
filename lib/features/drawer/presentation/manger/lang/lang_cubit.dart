fe03bf76523807cd3feec826dac85c64e3a9f0cd
khaled-dreat <dreatkhaled@gmail.com>
2024-03-03 17:59:52 +0200

    ADD check lang metod in lang cubit

--- lib/features/drawer/presentation/manger/lang/lang_cubit.dart
+++ lib/features/drawer/presentation/manger/lang/lang_cubit.dart
@@ -1,7 +1,14 @@
 import 'package:bloc/bloc.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../../core/utils/language/app_lang.dart';
+import '../../../../../core/utils/language/app_lang_config.dart';
 import '../../../../../core/utils/language/app_lang_key.dart';
+import '../../../../../core/utils/theme/app_color.dart';
+import '../../../../../core/utils/theme/app_theme.dart';
+import '../../../../../core/widgets/toast/app_toast.dart';
 
 part 'lang_state.dart';
 
@@ -15,4 +22,14 @@ enum ChoiceLang {
 
 class LangCubit extends Cubit<LangState> {
   LangCubit() : super(LangInitial());
+  // * UI Widget
+  ChoiceLang? lang;
+
+  // * check lang used easy locale
+  void checkLang(BuildContext context) {
+    emit(LangcheckLangSuccess(
+        lang: AppLang.currentLang(context) == kEn
+            ? ChoiceLang.en
+            : ChoiceLang.ar));
+  }
 }
--- lib/features/drawer/presentation/manger/lang/lang_state.dart
+++ lib/features/drawer/presentation/manger/lang/lang_state.dart
@@ -4,3 +4,11 @@ part of 'lang_cubit.dart';
 sealed class LangState {}
 
 final class LangInitial extends LangState {}
+
+final class LangcheckLangSuccess extends LangState {
+  final ChoiceLang lang;
+
+  LangcheckLangSuccess({required this.lang});
+}
+
+final class LangcheckLangFailure extends LangState {}
