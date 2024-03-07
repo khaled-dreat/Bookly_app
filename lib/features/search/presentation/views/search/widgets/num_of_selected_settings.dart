018d9e9e06a3d68e12e99645f00eb6c865cf71d8
khaled-dreat <dreatkhaled@gmail.com>
2024-03-07 20:37:54 +0200

    EDIT Add BlocBuilder

--- lib/features/search/presentation/views/search/widgets/num_of_selected_settings.dart
+++ lib/features/search/presentation/views/search/widgets/num_of_selected_settings.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../manger/category_books/category_books_cubit.dart';
 
 class NumOfSelectedSettings extends StatelessWidget {
   const NumOfSelectedSettings({
@@ -8,25 +11,29 @@ class NumOfSelectedSettings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      right: 5,
-      bottom: 25,
-      child: Container(
-        alignment: Alignment.topCenter,
-        height: 13.h,
-        width: 13.w,
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: const Text(
-          "1",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 12,
+    return BlocBuilder<IconButtonCubit, int?>(
+      builder: (context, state) {
+        return Positioned(
+          right: 5,
+          bottom: 25,
+          child: Container(
+            alignment: Alignment.topCenter,
+            height: 13.h,
+            width: 13.w,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              state.toString(),
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 12,
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
