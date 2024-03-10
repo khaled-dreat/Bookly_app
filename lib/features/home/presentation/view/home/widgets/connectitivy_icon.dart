ac503d3a8633934a9ccaf1bfbba628c6beb74c55
khaled-dreat <dreatkhaled@gmail.com>
2024-03-10 22:23:19 +0200

    EDIT refactore code

--- lib/features/home/presentation/view/home/widgets/connectitivy_icon.dart
+++ lib/features/home/presentation/view/home/widgets/connectitivy_icon.dart
@@ -1,22 +1,41 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
-class ConnectitivyIcon extends StatelessWidget {
+import '../../../../../../core/api/api_service.dart';
+
+class ConnectitivyIcon extends StatefulWidget {
   const ConnectitivyIcon({
     super.key,
   });
+
+  @override
+  State<ConnectitivyIcon> createState() => _ConnectitivyIconState();
+}
+
+class _ConnectitivyIconState extends State<ConnectitivyIcon> {
+  @override
+  void initState() {
+    super.initState();
+    ApiService.checkConnectitivy();
+  }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<ConnectivityResult>(
         stream: Connectivity().onConnectivityChanged,
         builder: (context, snapshot) {
-          return snapshot.data == ConnectivityResult.none
-              ? const Icon(
-                  Icons.wifi_off,
-                  color: Colors.red,
-                )
-              : const SizedBox();
+          if (snapshot.data == ConnectivityResult.none ||
+              snapshot.data == null) {
+            return const Icon(
+              Icons.wifi_off,
+              color: Colors.red,
+            );
+          } else {
+            return const SizedBox();
+          }
         });
   }
 }
--- lib/features/home/presentation/view/home/widgets/coustom_appbar.dart
+++ lib/features/home/presentation/view/home/widgets/coustom_appbar.dart
@@ -1,25 +1,11 @@
 import 'package:clean_arch_bookly_app/core/api/api_service.dart';
 import 'package:clean_arch_bookly_app/features/home/presentation/view/home/widgets/connectitivy_icon.dart';
 import 'package:clean_arch_bookly_app/features/home/presentation/view/home/widgets/srh_icon.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
-class CoustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+class CoustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CoustomAppBar({super.key});
-
-  @override
-  State<CoustomAppBar> createState() => _CoustomAppBarState();
-
-  @override
-  Size get preferredSize =>
-      Size.fromHeight(AppBar().preferredSize.height * 0.9);
-}
-
-class _CoustomAppBarState extends State<CoustomAppBar> {
-  @override
-  void initState() {
-    super.initState();
-    ApiService.checkConnectitivy();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,4 +16,8 @@ class _CoustomAppBarState extends State<CoustomAppBar> {
       ],
     );
   }
+
+  @override
+  Size get preferredSize =>
+      Size.fromHeight(AppBar().preferredSize.height * 0.9);
 }
