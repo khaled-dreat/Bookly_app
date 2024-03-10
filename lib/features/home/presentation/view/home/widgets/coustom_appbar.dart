64f13d98a9a3c5880d8be5c6e2c85c4b443cd965
khaled-dreat <dreatkhaled@gmail.com>
2024-03-10 21:19:44 +0200

    ADD checkConnectitivy

--- lib/features/home/presentation/view/home/widgets/coustom_appbar.dart
+++ lib/features/home/presentation/view/home/widgets/coustom_appbar.dart
@@ -1,3 +1,5 @@
+import 'package:clean_arch_bookly_app/core/api/api_service.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,13 +8,38 @@ import '../../../../../../core/utils/routes/app_routes.dart';
 import '../../../../../favorite/presentation/view/favorite_view.dart';
 import '../../../../../search/presentation/views/search/search_view.dart';
 
-class CoustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+class CoustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   const CoustomAppBar({super.key});
+
+  @override
+  State<CoustomAppBar> createState() => _CoustomAppBarState();
+
+  @override
+  Size get preferredSize =>
+      Size.fromHeight(AppBar().preferredSize.height * 0.9);
+}
+
+class _CoustomAppBarState extends State<CoustomAppBar> {
+  @override
+  void initState() {
+    super.initState();
+    ApiService.checkConnectitivy();
+  }
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       actions: [
+        StreamBuilder<ConnectivityResult>(
+            stream: Connectivity().onConnectivityChanged,
+            builder: (context, snapshot) {
+              return snapshot.data == ConnectivityResult.none
+                  ? const Icon(
+                      Icons.wifi_off,
+                      color: Colors.red,
+                    )
+                  : const SizedBox();
+            }),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: IconButton(
@@ -24,8 +51,4 @@ class CoustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       ],
     );
   }
-
-  @override
-  Size get preferredSize =>
-      Size.fromHeight(AppBar().preferredSize.height * 0.9);
 }
