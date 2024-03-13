6b296a81e6e771c1176f31eed7b03f937c6c3bc4
khaled-dreat <dreatkhaled@gmail.com>
2024-03-13 02:49:20 +0200

    EDIT refactor code

--- lib/features/webview/web_view.dart
+++ lib/features/webview/web_view.dart
@@ -1,9 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import '../../core/utils/dime/app_dime.dart';
 import '../../core/utils/language/app_lang_key.dart';
-import '../../core/widgets/loading/app_loading.dart';
 
 class PageWebView extends StatefulWidget {
   const PageWebView({super.key, required this.url, this.title});
@@ -34,24 +32,10 @@ class _PageWebViewState extends State<PageWebView> {
       appBar: AppBar(title: Text(widget.title ?? AppLangKey.terms.tr())),
       body: SafeArea(
         child: Stack(
-          // alignment: AlignmentDirectional.center,
           children: [
             WebViewWidget(
               controller: webViewController,
-
-              // initialUrl: widget.url,
-              // javascriptMode: JavaScriptMode.unrestricted,
-              // backgroundColor: AppTheme.themeColor(context),
-              // onPageFinished: (url) {
-              //   isLoading = false;
-              //   setState(() {});
-              // },
             ),
-            //if (isLoading)
-            //  Center(
-            //    child:
-            //        AppLoading(loading: TypeLoading.webview, size: AppDime.xxl),
-            //  ),
           ],
         ),
       ),
